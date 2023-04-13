ea55a7738d640537b791ba1c49d97af63ce44de3
MATEUS FELIPE DIAS <0058564@IFMG.EDU.BR>
2023-04-13 17:51:54 -0300

    funcional

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://https://api.api-futebol.com.br/v1/",
-    Headers:{
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + import.meta.env.VITE_TEST_KEY
+    baseURL: 'https://api.api-futebol.com.br/v1/',
+    headers: {
+        'Authorization': 'Bearer ' + import.meta.env.VITE_TEST_KEY, // Chave de acesso da API Futebol 
+        'Content-Type': 'application/json'
     }
+});
 
-    
-})
+const campeonato_id = 10; // ID referente ao campeonato que deseja consultar (Brasileirão)
 
-const campeonato_id = 10;
-export const getCampeonato = async  (page) =>{
+export const getCampeonato = async (page) => {
     try {
         const response = await api.get(`campeonatos/${campeonato_id}/${page}`);
         return response.data;
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
-}
+};
