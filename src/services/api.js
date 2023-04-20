d27d06562f1e51ef11a7c2fbdb3e7f63c78d808d
MATEUS FELIPE DIAS <0058564@ifmg.edu.br>
2023-04-20 17:30:30 -0300

    completo

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'https://api.api-futebol.com.br/v1/',
     headers: {
-        'Authorization': 'Bearer ' + import.meta.env.VITE_TEST_KEY, // Chave de acesso da API Futebol 
+        'Authorization': 'Bearer ' + import.meta.env.VITE_LIVE_KEY, // Chave de acesso da API Futebol 
         'Content-Type': 'application/json'
     }
 });
